4b50476fccbb466a12da9896b3184d5136859f24
imlk <imlk.geek@gmail.com>
2020-07-02 13:39:18 +0800

    feat: add publish and forward

--- runnet.sh
+++ runnet.sh
@@ -1,139 +1,227 @@
+outer_addr=''
+inner_addr=''
+net_ns_name="runnet$$"
+veth_outer_name="runnet$$_vo"
+veth_inner_name="runnet$$_vi"
+out_interface=''
 
-OUTER_ADDR=''
-INNER_ADDR=''
-NET_NS_NAME="runnet$$"
-VETH_OUTER_NAME="runnet_vo$$"
-VETH_INNER_NAME="runnet_vi$$"
-OUT_INTERFACE=''
+need_internet=0
+cmd_user=
+publish_list=()
+forward_list=()
 
-# sudo ip netns exec ${NET_NS_NAME} sudo -u imlk bash
+# sudo ip netns exec ${net_ns_name} sudo -u imlk bash
 
+# --forward=7474:7474
 # --publish=7474:7474
-# -u root
+# --user=root
 # --internet
 # --out-if=
-
+# host.docker.internal
 
 in_subnet() {
     local subnet mask subnet_split ip_split subnet_mask subnet_start subnet_end ip rval
     local readonly BITMASK=0xFFFFFFFF
 
-    IFS=/ read subnet mask <<< "${1}"
-    IFS=. read -a subnet_split <<< "${subnet}"
-    IFS=. read -a ip_split <<< "${2}"
+    IFS=/ read subnet mask <<<"${1}"
+    IFS=. read -a subnet_split <<<"${subnet}"
+    IFS=. read -a ip_split <<<"${2}"
 
-    subnet_mask=$(($BITMASK<<$((32-$mask)) & $BITMASK))
+    subnet_mask=$(($BITMASK << $((32 - $mask)) & $BITMASK))
     subnet_start=$((${subnet_split[0]} << 24 | ${subnet_split[1]} << 16 | ${subnet_split[2]} << 8 | ${subnet_split[3]} & ${subnet_mask}))
     subnet_end=$(($subnet_start | ~$subnet_mask & ${BITMASK}))
     ip=$((${ip_split[0]} << 24 | ${ip_split[1]} << 16 | ${ip_split[2]} << 8 | ${ip_split[3]} & ${BITMASK}))
 
-    (( $ip >= $subnet_start )) && (( $ip <= $subnet_end )) && rval=0 || rval=1
+    (($ip >= $subnet_start)) && (($ip <= $subnet_end)) && rval=0 || rval=1
     return ${rval}
 }
 
-
-setup_addr(){
+setup_addr() {
     local ip ok
     for ip_num in {0..255}; do
         ip="192.168.${ip_num}.1"
         ok=1
-        for subnet in `ip addr | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9][0-9]?\b"`; do
-            in_subnet ${subnet} ${ip} && { ok=0; break; }
+        for subnet in $(ip addr | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9][0-9]?\b"); do
+            in_subnet ${subnet} ${ip} && {
+                ok=0
+                break
+            }
         done
         if [[ ${ok} -eq 1 ]]; then
-            OUTER_ADDR="192.168.${ip_num}.1"
-            INNER_ADDR="192.168.${ip_num}.2"
+            outer_addr="192.168.${ip_num}.1"
+            inner_addr="192.168.${ip_num}.2"
             return
         fi
     done
     error "Unable to find unused subnet in range 192.168.0.0 - 192.168.255.0, please customize it" || exit 1
 }
 
-
-setup_interface(){
+setup_interface() {
     local dev
-    dev=`ip -4 route list 0/0 | cut -d ' ' -f 5`
+    dev=$(ip -4 route list 0/0 | cut -d ' ' -f 5)
     if [[ ${dev} == "" ]]; then
         error "Can not identify the default gateway interface. You must specify it by --out-if" || exit 1
     fi
-    OUT_INTERFACE=$dev
+    out_interface=$dev
 }
 
 # start up env
-start_up(){
+start_up() {
     # add net namespace
-    ip netns add ${NET_NS_NAME}
+    ip netns add ${net_ns_name}
 
     # add veth
-    ip link add ${VETH_OUTER_NAME} type veth peer name ${VETH_INNER_NAME}
+    ip link add ${veth_outer_name} type veth peer name ${veth_inner_name}
     # setup veth_outer
-    ip link set ${VETH_OUTER_NAME} up
-    ip addr add ${OUTER_ADDR}/24 dev ${VETH_OUTER_NAME}
+    ip link set ${veth_outer_name} up
+    ip addr add ${outer_addr}/24 dev ${veth_outer_name}
 
     # setup veth_inner
-    ip link set ${VETH_INNER_NAME} netns ${NET_NS_NAME}
-    ip netns exec ${NET_NS_NAME} ip link set ${VETH_INNER_NAME} up
-    ip netns exec ${NET_NS_NAME} ip addr add ${INNER_ADDR}/24 dev ${VETH_INNER_NAME}
+    ip link set ${veth_inner_name} netns ${net_ns_name}
+    ip netns exec ${net_ns_name} ip link set ${veth_inner_name} up
+    ip netns exec ${net_ns_name} ip addr add ${inner_addr}/24 dev ${veth_inner_name}
     # enable loopback
-    ip netns exec ${NET_NS_NAME} ip link set lo up
+    ip netns exec ${net_ns_name} ip link set lo up
 
-    # add default route
-    ip netns exec ${NET_NS_NAME} ip route add default via ${OUTER_ADDR}
-    # enable NAT
-    bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-    iptables -t nat -A POSTROUTING -s ${INNER_ADDR}/24 -o ${OUT_INTERFACE} -j MASQUERADE
-    iptables -t filter -A FORWARD -i ${OUT_INTERFACE} -o ${VETH_OUTER_NAME} -j ACCEPT
-    iptables -t filter -A FORWARD -o ${OUT_INTERFACE} -i ${VETH_OUTER_NAME} -j ACCEPT
+    if [[ ${need_internet} -eq 1 ]]; then
+        # add default route
+        ip netns exec ${net_ns_name} ip route add default via ${outer_addr}
+        # enable NAT
+        bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+        iptables -t nat -A POSTROUTING -s ${inner_addr}/24 -o ${out_interface} -j MASQUERADE
+        iptables -t filter -A FORWARD -i ${out_interface} -o ${veth_outer_name} -j ACCEPT
+        iptables -t filter -A FORWARD -o ${out_interface} -i ${veth_outer_name} -j ACCEPT
+    fi
 }
-
 
 # shut down env
-shut_down(){
-    # disable NAT
-    iptables -t nat -D POSTROUTING -s ${INNER_ADDR}/24 -o ${OUT_INTERFACE} -j MASQUERADE
-    iptables -t filter -D FORWARD -i ${OUT_INTERFACE} -o ${VETH_OUTER_NAME} -j ACCEPT
-    iptables -t filter -D FORWARD -o ${OUT_INTERFACE} -i ${VETH_OUTER_NAME} -j ACCEPT
-
+shut_down() {
+    if [[ ${need_internet} -eq 1 ]]; then
+        # disable NAT
+        iptables -t nat -D POSTROUTING -s ${inner_addr}/24 -o ${out_interface} -j MASQUERADE
+        iptables -t filter -D FORWARD -i ${out_interface} -o ${veth_outer_name} -j ACCEPT
+        iptables -t filter -D FORWARD -o ${out_interface} -i ${veth_outer_name} -j ACCEPT
+    fi
     # delete veth
-    ip link delete ${VETH_OUTER_NAME}
+    ip link delete ${veth_outer_name}
     # delete net namespace
-    ip netns delete ${NET_NS_NAME}
+    ip netns delete ${net_ns_name}
 }
 
+setup_port_mapping() {
+    for publish in ${publish_list[@]}; do
+        type=
+        if [[ ${type} == */* ]]; then
+            type=${publish##*/}
+        fi
+        type=${type:-"tcp"}
+        port_map=${publish%/*}
+        port_src=${port_map%:*}
+        port_dest=${inner_addr}:${port_map##*:}
+        info "publish: host[0.0.0.0:${port_src}]\t--(${type})->\tcontainer[${port_dest}]"
+        socat -lf/dev/null ${type}-listen:${port_src},fork ${type}:${port_dest} &
+    done
 
-error(){
-    echo "[error] $1"
+    for forward in ${forward_list[@]}; do
+        type=
+        if [[ ${type} == */* ]]; then
+            type=${forward##*/}
+        fi
+        type=${type:-"tcp"}
+        port_map=${forward%/*}
+        port_src=${port_map%:*}
+        port_dest=${port_map##*:}
+        if [[ ${port_src} != *:* ]]; then
+            port_src=127.0.0.1:${port_src}
+        fi
+        info "forward: host[${port_src}]\t<-(${type})--\tcontainer[0.0.0.0:${port_dest}]"
+        unix_file="/tmp/runnet$$_${type}_${port_src}_${port_dest}"
+        socat -lf/dev/null unix-listen:\"${unix_file}\",fork ${type}:${port_src} &
+        ip netns exec ${net_ns_name} socat -lf/dev/null ${type}-listen:${port_dest},fork unix-connect:\"${unix_file}\" &
+    done
 }
 
-warning(){
-    echo "[warning] $1"
+kill_this(){
+    shut_down
+    pkill -P $$
 }
 
-usage(){
+error() {
+    echo -e "[error] $1"
+}
+
+info() {
+    echo -e "[info] $1"
+}
+
+warning() {
+    echo -e "[warning] $1"
+}
+
+usage() {
     echo "This is usage part"
 }
 
-
 if [[ ${EUID} -ne 0 ]]; then
-    error "This script must be run as root"; exit 1
+    error "This script must be run as root"
+    exit 1
 fi
 
 if [[ $# -eq 0 ]]; then
-    usage; exit 1
+    usage
+    exit 1
 fi
 
+while true; do
+    case $1 in
+    --internet) # need internet access
+        need_internet=1
+        shift
+        ;;
+    --user=*)
+        cmd_user=${1:7}
+        shift
+        ;;
+    --out-if=*)
+        out_interface=${1:9}
+        shift
+        ;;
+    --publish=*:*)
+        publish_list+=(${1:10})
+        shift
+        ;;
+    --forward=*:*)
+        forward_list+=(${1:10})
+        shift
+        ;;
+    -)
+        shift
+        break
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
 setup_addr
-setup_interface
+
+[[ ${out_interface} == "" ]] && setup_interface
+
+trap kill_this EXIT
 
 start_up
+setup_port_mapping
+
 cmd="$*"
-if [[ ${SUDO_USER} != "" ]]; then
-    cmd="sudo -u ${SUDO_USER} ${cmd}"
+
+if [[ ${cmd_user} == "" ]]; then
+    cmd_user=${SUDO_USER}
+fi
+if [[ ${cmd_user} != "" ]]; then
+    cmd="sudo -u ${cmd_user} ${cmd}"
 else
     warning "\${SUDO_USER} is empty and cmd will run as root"
 fi
-ip netns exec ${NET_NS_NAME} ${cmd}
-
-shut_down
-
+ip netns exec ${net_ns_name} ${cmd}
 
